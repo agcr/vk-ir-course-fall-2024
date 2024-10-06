bd6387a8f2db5720dfbefd20e26cf31dea909f4f
agcr <agcr.dev@gmail.com>
2024-10-06 23:56:09 +0300

    Add indexing homework template

---
+++ homeworks/03-indexing/solution.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Indexing homework solution"""
+
+import argparse
+from timeit import default_timer as timer
+
+from nltk import tokenize
+
+def preprocess(text):
+    # Tokenize
+    tokenizer = tokenize.RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(text)
+
+    # Normalize
+    return [token.lower() for token in tokens]
+
+def main():
+    # Парсим опции командной строки
+    parser = argparse.ArgumentParser(description='Indexing homework solution')
+    parser.add_argument('--submission_file', help='output Kaggle submission file')
+    parser.add_argument('--build_index', action='store_true', help='force reindexing')
+    parser.add_argument('--index_dir', required=True, help='index directory')
+    parser.add_argument('data_dir', help='input data directory')
+    args = parser.parse_args()
+
+    # Будем измерять время работы скрипта
+    start = timer()
+
+    # Какой у нас режим: построения индекса или генерации сабмишна?
+    if args.build_index:
+        # Тут вы должны:
+        # - загрузить тексты документов из файла args.data_dir/vkmarco-docs.tsv
+        # - проиндексировать эти тексты, причем для разбиения текстов на слова (термины) надо воспользоваться функцией preprocess()
+        # - сохранить получивший обратный индекс в папку переданную через параметр args.index_dir
+        pass
+    else:
+        # Тут вы должны:
+        # - загрузить поисковые запросы из файла args.data_dir/vkmarco-doceval-queries.tsv
+        # - прогнать запросы через индекс и, для каждого запроса, найти все документы, в которых есть все слова (термины) из запроса.
+        # - для разбиения текстов запросов на слова тоже используем функцию preprocess()
+        # - сформировать ваш сабмишн, в котором для каждого объекта (пары запрос-документ) будет проставлена метка 1 (в документе есть все слова из запроса) или 0
+        #
+        # Для формирования сабмишна надо загрузить и использовать файлы args.data_dir/sample_submission.csv и args.data_dir/objects.csv
+        pass
+
+    # Репортим время работы скрипта
+    elapsed = timer() - start
+    print(f"finished, elapsed = {elapsed:.3f}")
+
+
+if __name__ == "__main__":
+    main()
