216043eb38f1e038e2e55b70dc4ac67d1f84a781
a-ct-seal <sitkina.alena2017@yandex.ru>
2024-12-19 17:07:55 +0300

    part of solution

--- homeworks/03-indexing/solution.py
+++ homeworks/03-indexing/solution.py
@@ -3,9 +3,28 @@
 """Indexing homework solution"""
 
 import argparse
+import os
+
+import pandas as pd
 from timeit import default_timer as timer
+from collections import defaultdict
 
 from nltk import tokenize
+
+def intersect(a, b):
+    res = []
+    pa = 0
+    pb = 0
+    while pa < len(a) and pb < len(b):
+        if a[pa] == b[pb]:
+            res.append(a[pa])
+            pa += 1
+            pb += 1
+        elif a[pa] < b[pb]:
+            pa += 1
+        else:
+            pb += 1
+    return res
 
 def preprocess(text):
     # Tokenize
@@ -31,12 +50,40 @@ def main():
     if args.build_index:
         # Тут вы должны:
         # - загрузить тексты документов из файла args.data_dir/vkmarco-docs.tsv
-        # - проиндексировать эти тексты, причем для разбиения текстов на слова (термины) надо воспользоваться функцией preprocess()
-        # - сохранить получивший обратный индекс в папку переданную через параметр args.index_dir
-        pass
+        filename = os.path.join(args.data_dir, "vkmarco-docs.tsv")
+        df = pd.read_csv(filename, sep='\t', names=['id', 'url', 'title', 'body'])
+        df['id'] = df['id'].apply(lambda x: int(x[1:]))
+        df['text'] = df['title'] + " " + df['body']
+        df.drop(['title', 'body', 'url'], axis=1, inplace=True)
+        df = df.set_index('id')
+        df.dropna(inplace=True)
+        dictionary = defaultdict(list)
+        for index, row in df.iterrows():
+            # - проиндексировать эти тексты, причем для разбиения текстов на слова (термины) надо воспользоваться функцией preprocess()
+            for word in preprocess(row.text):
+                dictionary[word].append(index)
+        dictionary = dict(dictionary)
+        for key in dictionary.keys():
+            dictionary[key].sort()
+        # - сохранить получивший обратный индекс в папку, переданную через параметр args.index_dir
+        with open(os.path.join(args.index_dir, "index"), 'w') as out:
+            out.write(str(len(dictionary)))
+            for key in dictionary.keys():
+                out.write(key)
+            for key in dictionary.keys():
+                out.write(' '.join(map(str, dictionary[key])))
     else:
         # Тут вы должны:
         # - загрузить поисковые запросы из файла args.data_dir/vkmarco-doceval-queries.tsv
+        with open(os.path.join(args.index_dir, "index"), 'r') as index_file:
+            pass
+        labels = dict()
+        with open(os.path.join(args.data_dir, "vkmarco-doceval-queries.tsv"), 'r') as infile:
+            line = infile.readline()
+            id, query = line.split(' ', 1)
+            id = int(id)
+
+
         # - прогнать запросы через индекс и, для каждого запроса, найти все документы, в которых есть все слова (термины) из запроса.
         # - для разбиения текстов запросов на слова тоже используем функцию preprocess()
         # - сформировать ваш сабмишн, в котором для каждого объекта (пары запрос-документ) будет проставлена метка 1 (в документе есть все слова из запроса) или 0
