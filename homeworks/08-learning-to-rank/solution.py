00a08d63af2a405ec4fbc8adebeb24f1af31557d
a-ct-seal <sitkina.alena2017@yandex.ru>
2024-12-21 19:51:37 +0300

    solution 1 try

--- homeworks/08-learning-to-rank/solution.py
+++ homeworks/08-learning-to-rank/solution.py
@@ -3,7 +3,25 @@
 """Learning to Rank homework solution"""
 
 import argparse
+import copy
+import os
 from timeit import default_timer as timer
+import pandas as pd
+import catboost
+
+def read_dataframe(args, filename):
+    df = pd.read_csv(os.path.join(args.data_dir, filename), sep=' ', header=None)
+    df.columns = ['target', 'id'] + [i for i in range(df.shape[1] - 2)]
+    df['id'] = df['id'].apply(lambda x: int(x.split(':')[1]))
+    return df
+
+
+def to_catboost_dataset(df):
+    y = df['target'].to_numpy()
+    q = df['id'].to_numpy().astype('uint32')
+    X = df.drop(columns=['target', 'id']).to_numpy()
+    return X, y, q
+
 
 def main():
     # Парсим опции командной строки
@@ -17,29 +35,76 @@ def main():
     # Будем измерять время работы скрипта
     start = timer()
 
-    # Тут вы, скорее всего, должны проинициализировать фиксированным seed'ом генератор случайных чисел для того, чтобы ваше решение было воспроизводимо.
-    # Например (зависит от того, какие конкретно либы вы используете):
-    #
-    # random.seed(42)
-    # np.random.seed(42)
-    # и т.п.
-
     # Какой у нас режим: обучения модели или генерации сабмишна?
     if args.train:
+        df_train = read_dataframe(args, 'train.txt')
+        df_val = read_dataframe(args, 'vali.txt')
+
+        X_train, y_train, q_train = to_catboost_dataset(df_train)
+        X_val, y_val, q_val = to_catboost_dataset(df_val)
+
+        pool_train = catboost.Pool(data=X_train, label=y_train, group_id=q_train)
+        pool_val = catboost.Pool(data=X_val, label=y_val, group_id=q_val)
+
+        EVAL_METRIC = 'NDCG:top=5;type=Exp'
+
+        DEFAULT_PARAMS = {
+            'iterations': 1000,
+            'early_stopping_rounds': 100,
+            'eval_metric': EVAL_METRIC,
+            'random_seed': 42,
+            'verbose': 10
+        }
+
+        def create_model(loss_function):
+            params = copy.deepcopy(DEFAULT_PARAMS)
+            # Temporary directory that is used by catboost to store additional information
+            catboost_info_dir = f"/tmp/catboost_info.{loss_function.lower()}"
+            params.update({
+                'loss_function': loss_function,
+                'train_dir': str(catboost_info_dir),
+            })
+            return catboost.CatBoost(params)
+
+        model = create_model('RMSE')
+        start = timer()
+        model.fit(pool_train, eval_set=pool_val, use_best_model=True)
+        elapsed = timer() - start
+        print(f"Model fit: num_trees = {model.tree_count_} elapsed = {elapsed:.3f}")
+
+        model.save_model(args.model_file)
+
         # Тут вы должны:
         # - загрузить датасет VKLR из папки args.data_dir
         # - обучить модель с использованием train- и dev-сплитов датасета
         # - при необходимости, подобрать гиперпараметры
         # - сохранить обученную модель в файле args.model_file
-        pass
     else:
+        model = catboost.CatBoost()
+        model.load_model(args.model_file)
+
+        df_test = read_dataframe(args, 'test.txt')
+
+        X_test, _, q_test = to_catboost_dataset(df_test)
+        pool_test = catboost.Pool(data=X_test, group_id=q_test)
+        y_predicted = model.predict(pool_test)
+
+        df_results = pd.DataFrame({
+            'QueryId': q_test,
+            'DocumentId': range(len(q_test)),
+            'predicted': y_predicted
+        })
+
+        df_results = df_results.sort_values(by=['QueryId', 'predicted'], ascending=[True, False])
+        df_results = df_results[['QueryId', 'DocumentId']]
+        df_results.to_csv(args.submission_file, index=False)
+
         # Тут вы должны:
         # - загрузить тестовую часть датасета VKLR из папки args.data_dir
         # - загрузить модель из файла args.model_file
         # - применить модель ко всем запросам и документам из test.txt
         # - переранжировать документы по каждому из запросов так, чтобы более релевантные шли сначала
         # - сформировать ваш сабмишн для заливки на Kaggle и сохранить его в файле args.submission_file
-        pass
 
     # Репортим время работы скрипта
     elapsed = timer() - start
