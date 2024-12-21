6afe8325c6764c6eeebd71989738708694ab99d6
samiaFife <368983@niuitmo.ru>
2024-12-21 14:58:57 +0300

    added hw3 learning to rank

--- homeworks/08-learning-to-rank/solution.py
+++ homeworks/08-learning-to-rank/solution.py
@@ -3,7 +3,56 @@
 """Learning to Rank homework solution"""
 
 import argparse
+import copy
+import os
 from timeit import default_timer as timer
+
+import catboost
+import numpy as np
+import pandas as pd
+import tqdm
+
+DEFAULT_PARAMS = {
+    'iterations': 5000,
+    'early_stopping_rounds': 100,
+    'eval_metric': 'NDCG:top=10;type=Exp',
+    'random_seed': 42,
+    'verbose': 10,
+    'task_type': 'GPU'
+}
+
+
+def create_model(loss_function):
+    params = copy.deepcopy(DEFAULT_PARAMS)
+    catboost_info_dir = f"/tmp/hw-08-learning-rank/catboost_info.{loss_function.lower()}"
+    params.update({
+        'loss_function': loss_function,
+        'train_dir': str(catboost_info_dir),
+    })
+    return catboost.CatBoost(params)
+
+
+def to_catboost_dataset(df):
+    y = df['label'].to_numpy()
+    q = df['qid'].to_numpy().astype('uint32')
+    X = df.drop(columns=['label', 'qid']).to_numpy()
+    return (X, y, q)
+
+
+def proceed_file(path):
+    df = pd.DataFrame()
+    total_lines = sum(1 for _ in open(path))
+    chunk_size = 15000
+    for chunk in tqdm.tqdm(pd.read_csv(path, sep=' ', header=None, chunksize=chunk_size),
+                           total=total_lines // chunk_size,
+                           desc="Processing file chunks"):
+        chunk.columns = ['label', 'qid'] + [str(i) for i in range(1, 574)]
+        chunk['qid'] = chunk['qid'].str.split(':').str[1].astype(int)
+        for col in chunk.columns[2:]:
+            chunk[col] = chunk[col].str.split(':').str[1].astype(float)
+        df = pd.concat([df, chunk], ignore_index=True)
+    return df
+
 
 def main():
     # Парсим опции командной строки
@@ -23,7 +72,7 @@ def main():
     # random.seed(42)
     # np.random.seed(42)
     # и т.п.
-
+    np.random.seed(42)
     # Какой у нас режим: обучения модели или генерации сабмишна?
     if args.train:
         # Тут вы должны:
@@ -31,7 +80,23 @@ def main():
         # - обучить модель с использованием train- и dev-сплитов датасета
         # - при необходимости, подобрать гиперпараметры
         # - сохранить обученную модель в файле args.model_file
-        pass
+        df_train = proceed_file(args.data_dir + '/train.txt')
+        df_vali = proceed_file(args.data_dir + '/vali.txt')
+
+        X_train, y_train, q_train = to_catboost_dataset(df_train)
+        X_vali, y_vali, q_vali = to_catboost_dataset(df_vali)
+
+        pool_train = catboost.Pool(data=X_train, label=y_train, group_id=q_train)
+        pool_vali = catboost.Pool(data=X_vali, label=y_vali, group_id=q_vali)
+
+        model = create_model('YetiRank')
+
+        start_fit = timer()
+        model.fit(pool_train, eval_set=pool_vali, use_best_model=True)
+        elapsed = timer() - start_fit
+        print(f"Model fit: num_trees = {model.tree_count_} elapsed = {elapsed:.3f}")
+
+        model.save_model(args.model_file)
     else:
         # Тут вы должны:
         # - загрузить тестовую часть датасета VKLR из папки args.data_dir
@@ -39,7 +104,20 @@ def main():
         # - применить модель ко всем запросам и документам из test.txt
         # - переранжировать документы по каждому из запросов так, чтобы более релевантные шли сначала
         # - сформировать ваш сабмишн для заливки на Kaggle и сохранить его в файле args.submission_file
-        pass
+        df_test = proceed_file(args.data_dir + '/test.txt')
+        X_test, y_test, q_test = to_catboost_dataset(df_test)
+        pool_test = catboost.Pool(data=X_test, label=y_test, group_id=q_test)
+
+        model = catboost.CatBoost()
+        model.load_model(args.model_file)
+
+        y_test_labels = model.predict(pool_test)
+        df_test['label'] = y_test_labels
+        df_test['ind'] = df_test.index
+        df_sorted = df_test.sort_values(by=['qid', 'label'], ascending=[True, False])
+
+        res = df_sorted[['qid', 'ind']].rename(columns={'qid': 'QueryId', 'ind': 'DocumentId'})
+        res.to_csv(args.submission_file, index=False)
 
     # Репортим время работы скрипта
     elapsed = timer() - start
