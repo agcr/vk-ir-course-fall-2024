7e39e092e85ac969c49be728a1567a5af921cbc5
Arsenii Pimenov <arsenii.pimenov@raftds.com>
2024-12-22 22:22:46 +0300

    catboost GPU compute

--- homeworks/08-learning-to-rank/solution.py
+++ homeworks/08-learning-to-rank/solution.py
@@ -4,6 +4,7 @@
 
 import argparse
 import copy
+import json
 import os
 from timeit import default_timer as timer
 
@@ -15,12 +16,14 @@ from catboost import datasets, utils
 EVAL_METRIC = 'NDCG:top=5;type=Exp'
 
 DEFAULT_PARAMS = {
-    'iterations': 1000,
-    'early_stopping_rounds': 100,
+    'iterations': 30000,
+    'early_stopping_rounds': 4000,
     'eval_metric': EVAL_METRIC,
     'random_seed': 52,
-    'verbose': 10
+    'verbose': 1000,
+    'task_type': 'GPU'
 }
+
 
 def generate_column_names(num_features):
     columns = ['label', 'qid']
@@ -29,16 +32,42 @@ def generate_column_names(num_features):
         columns.append(column)
     return columns
 
+
 def read_dataframe(data_dir, filename):
     df = pd.read_csv(os.path.join(data_dir, filename), sep=' ', header=None)
+
     num_features = df.shape[1] - 2
     df.columns = generate_column_names(num_features)
+
+    # qid 0:1 -> qid 1
     df['qid'] = df['qid'].apply(lambda x: int(x.split(':')[1]))
+    
+    # 0:1 -> feature_i 1
     for i in range(num_features):
         feature_col = f"feature_{i+1}"
         df[feature_col] = df[feature_col].apply(lambda x: float(x.split(':')[1]))
     
     return df
+
+
+def load_or_create_dataframe(data_dir, input_filename, processed_filename):
+    processed_filepath = os.path.join(data_dir, processed_filename)
+    
+    # Проверка, существует ли предварительно сохранённый CSV
+    if os.path.exists(processed_filepath):
+        print(f"Loading processed data from {processed_filepath}")
+        return pd.read_csv(processed_filepath)
+    
+    # Если CSV не существует, загружаем и обрабатываем оригинальный файл
+    print(f"Processing raw data from {input_filename}")
+    df = read_dataframe(data_dir, input_filename)
+    
+    # Сохраняем обработанный DataFrame для ускорения последующих загрузок
+    print(f"Saving processed data to {processed_filepath}")
+    df.to_csv(processed_filepath, index=False)
+    
+    return df
+
 
 def to_catboost_dataset(df):
     y = df['label'].to_numpy()
@@ -46,10 +75,11 @@ def to_catboost_dataset(df):
     X = df.drop(columns=['label', 'qid']).to_numpy()
     return (X, y, q)
 
+
 def create_model(loss_function):
     params = copy.deepcopy(DEFAULT_PARAMS)
 
-    # Temporary directory that is used by catboost to store additional information
+    # Временная директория для хранения информации catboost во время train
     catboost_info_dir = f"homeworks/08-learning-to-rank/catboost_info.{loss_function.lower()}"
 
     params.update({
@@ -58,14 +88,13 @@ def create_model(loss_function):
     })
     return catboost.CatBoost(params)
 
+
 def compute_metrics(y_true, y_hat, q):
-    # List of metrics to evaluate
-    eval_metrics = ['NDCG:top=10;type=Exp', 'DCG:top=10;type=Exp', 'MAP:top=10']
+    # Список метрик для подсчета
+    eval_metrics = ['NDCG:top=5;type=Exp', 'DCG:top=5;type=Exp', 'MAP:top=5']
     
     for eval_metric in eval_metrics:
         scores = utils.eval_metric(y_true, y_hat, eval_metric, group_id=q)
-    
-        # Print scores
         print(f"metric = {eval_metric} score = {scores[0]:.3f}")
 
 
@@ -95,23 +124,19 @@ def main():
         # - обучить модель с использованием train- и dev-сплитов датасета
         # - при необходимости, подобрать гиперпараметры
         # - сохранить обученную модель в файле args.model_file
-        df_train = read_dataframe(args.data_dir, 'train.txt')
-        df_val = read_dataframe(args.data_dir, 'vali.txt')
+        df_train = load_or_create_dataframe(args.data_dir, 'train.txt', 'train_processed.csv')
+        df_val = load_or_create_dataframe(args.data_dir, 'vali.txt', 'vali_processed.csv')
+        
         X_train, y_train, q_train = to_catboost_dataset(df_train)
         X_val, y_val, q_val = to_catboost_dataset(df_val)
-
-        pool_train = catboost.Pool(data=X_train, label=y_train, group_id=q_train)
+        pool_train = catboost.Pool(data=X_train, label=y_train, group_id=q_train, )
         pool_val = catboost.Pool(data=X_val, label=y_val, group_id=q_val)
 
         model = create_model('YetiRank')
-
-        start = timer()
         model.fit(pool_train, eval_set=pool_val, use_best_model=True)
-        elapsed = timer() - start
-
         model.save_model(args.model_file)
         compute_metrics(y_val, model.predict(pool_val), q_val)
-        print(f"Model fit: num_trees = {model.tree_count_}\nModel train time = {elapsed:.3f}")
+        print(f"Model fit: num_trees = {model.tree_count_}")
     else:
         # Тут вы должны:
         # - загрузить тестовую часть датасета VKLR из папки args.data_dir
@@ -121,12 +146,12 @@ def main():
         # - сформировать ваш сабмишн для заливки на Kaggle и сохранить его в файле args.submission_file
         model = catboost.CatBoost()
         model.load_model(args.model_file)
-        df_test = read_dataframe(args.data_dir, 'test.txt')
 
+        df_test = load_or_create_dataframe(args.data_dir, 'test.txt', 'test_processed.csv')
         X_test, y_test, q_test = to_catboost_dataset(df_test)
+        
         pool_test = catboost.Pool(data=X_test, group_id=q_test)
         y_pred = model.predict(pool_test)
-
         compute_metrics(y_test, y_pred, q_test)
 
         df_results = pd.DataFrame({
